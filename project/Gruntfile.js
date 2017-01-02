f43b50b3d7549252713420840c9a3f068d8bdf60
Greg Sarault <gregsarault@gmail.com>
2017-01-02 17:37:29 -0500

    removed the cacheing renaming

--- project/Gruntfile.js
+++ project/Gruntfile.js
@@ -259,10 +259,7 @@ module.exports = function (grunt) {
     filerev: {
       dist: {
         src: [
-          '<%= yeoman.dist %>/scripts/{,*/}*.js',
-          '<%= yeoman.dist %>/styles/{,*/}*.css',
-          '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
-          '<%= yeoman.dist %>/styles/fonts/*'
+          
         ]
       }
     },
