8cc75ce1a2b4d59137874e246284a79589e004c4
Greg Sarault <gregsarault@gmail.com>
2017-01-09 09:39:31 -0500

    Adding resume to site

---
+++ project/app/scripts/controllers/resumeCtrl.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gsarault44githubioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gsarault44githubioApp
+ */
+angular.module('gsarault44githubioApp').controller('ResumeCtrl', function () {
+
+});
